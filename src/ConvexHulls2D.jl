2c1d566aae1a8af616ea10a8335998824741008b
Twan Koolen <koolen.twan@gmail.com>
2019-01-23 17:40:36 -0500

    Basics.

--- src/ConvexHulls2D.jl
+++ src/ConvexHulls2D.jl
@@ -1,5 +1,98 @@
 module ConvexHulls2D
 
-greet() = print("Hello World!")
+export
+    ConvexHull2D,
+    vertices,
+    num_vertices,
+    area,
+    is_ccw_and_strongly_convex
+
+using LinearAlgebra
+using StaticArrays
+using StaticArrays: arithmetic_closure
+
+const PointLike{T} = StaticVector{2, T}
+
+unpack(v::PointLike) = @inbounds return v[1], v[2]
+
+struct CCWStronglyConvexError <: Exception
+end
+function Base.showerror(io::IO, e::CCWStronglyConvexError)
+    print(io, "Points are not in counterclockwise order or do not represent a strongly convex set")
+end
+
+struct ConvexHull2D{T, P<:PointLike{T}, V<:AbstractVector{P}}
+    vertices::V
+
+    function ConvexHull2D(vertices::V; check=true) where {T, P<:PointLike{T}, V<:AbstractVector{P}}
+        if check
+            is_ccw_and_strongly_convex(vertices) || throw(CCWStronglyConvexError())
+        end
+        new{T, P, V}(vertices)
+    end
+end
+
+@inline vertices(hull::ConvexHull2D) = hull.vertices
+@inline num_vertices(hull::ConvexHull2D) = length(vertices(hull))
+@inline Base.isempty(hull::ConvexHull2D) = num_vertices(hull) > 0
+
+@inline function cross2(v1::StaticVector{2}, v2::StaticVector{2})
+    x1, y1 = unpack(v1)
+    x2, y2 = unpack(v2)
+    x1 * y2 - y1 * x2
+end
+
+function area(hull::ConvexHull2D{T}) where T
+    # https://en.wikipedia.org/wiki/Shoelace_formula
+    vertices = hull.vertices
+    n = length(vertices)
+    n > 1 || return zero(arithmetic_closure(T))
+    @inbounds begin
+        ret = cross2(vertices[n], vertices[1])
+        @simd for i in Base.OneTo(n - 1)
+            ret += cross2(vertices[i], vertices[i + 1])
+        end
+        return abs(ret) / 2
+    end
+end
+
+function is_ordered_and_convex(vertices::AbstractVector{<:PointLike}, op::O) where {T, O}
+    n = length(vertices)
+    n <= 2 && return true
+    @inbounds begin
+        δprev = vertices[n] - vertices[n - 1]
+        δnext = vertices[1] - vertices[n]
+        for i in Base.OneTo(n - 1)
+            op(cross2(δprev, δnext), 0) || return false
+            δprev = δnext
+            δnext = vertices[i + 1] - vertices[i]
+        end
+        return op(cross2(δprev, δnext), 0)
+    end
+end
+
+is_ccw_and_strongly_convex(vertices::AbstractVector{<:PointLike}) = is_ordered_and_convex(vertices, >)
+
+function Base.in(point::PointLike, hull::ConvexHull2D)
+    vertices = hull.vertices
+    n = length(vertices)
+    @inbounds begin
+        if n === 1
+            return point == hull.vertices[1]
+        elseif n === 2
+            p′ = point - vertices[1]
+            δ = vertices[2] - vertices[1]
+            cross2(p′, δ) == 0 && 0 <= p′ ⋅ δ <= sum(x -> x^2, δ)
+        else
+            op = <= # may want to put this in a ConvexHull2D type parameter
+            δ = vertices[1] - vertices[n]
+            for i in Base.OneTo(n - 1)
+                op(cross2(point - vertices[i], δ), 0) || return false
+                δ = vertices[i + 1] - vertices[i]
+            end
+            return op(cross2(point - vertices[n], δ), 0)
+        end
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,88 @@
-using ConvexHulls2D
-using Test
+module ConvexHulls2DTest
 
-@testset "ConvexHulls2D.jl" begin
-    # Write your own tests here.
+using ConvexHulls2D
+using StaticArrays
+using Test
+using Random
+using LinearAlgebra
+
+const Point{T} = SVector{2, T}
+
+@testset "is_ccw_and_strongly_convex" begin
+    @test is_ccw_and_strongly_convex([Point(1, 2)])
+    @test is_ccw_and_strongly_convex([Point(1, 2), Point(3, 4)])
+    @test is_ccw_and_strongly_convex([Point(1, 2), Point(3, 4), Point(2, 4)])
+    @test !is_ccw_and_strongly_convex([Point(1, 2), Point(3, 4), Point(2, 3)]) # on a line
+
+    v1 = Point(0, 0)
+    v2 = Point(1, 0)
+    v3 = Point(1, 1)
+    v4 = Point(0, 1)
+    vertices = [v1, v2, v3, v4]
+    for i in 0 : 4
+        shifted = circshift(vertices, i)
+        @test is_ccw_and_strongly_convex(shifted)
+        @test !is_ccw_and_strongly_convex(reverse(shifted))
+    end
+
+    for i in eachindex(vertices)
+        for j in eachindex(vertices)
+            if i != j
+                vertices′ = copy(vertices)
+                vertices′[i] = vertices[j]
+                vertices′[j] = vertices[i]
+                @test !is_ccw_and_strongly_convex(vertices′)
+            end
+        end
+    end
 end
+
+@testset "area" begin
+    @test area(ConvexHull2D(SVector((SVector(1, 1),)))) == 0
+
+    @test area(ConvexHull2D(SVector(SVector(1, 1), SVector(2, 3)))) == 0
+
+    triangle = ConvexHull2D(SVector(Point(1, 1), Point(2, 1), Point(3, 3)))
+    @test area(triangle) == 1.0
+
+    square = ConvexHull2D(SVector(Point(1, 1), Point(4, 1), Point(4, 3), Point(1, 3)))
+    @test area(square) == 3 * 2
+end
+
+@testset "in" begin
+    @testset "point" begin
+        rng = MersenneTwister(1)
+        p = SVector(1, 1)
+        C = ConvexHull2D(SVector((p,)))
+        @test p ∈ C
+        @test Float64.(p) ∈ C
+        for i in 1 : 10
+            @test p + SVector(randn(rng), randn(rng)) ∉ C
+        end
+    end
+
+    @testset "line segment" begin
+        p1 = SVector(1, 1)
+        p2 = SVector(3, 5)
+        C = ConvexHull2D([p1, p2])
+        @test p1 ∈ C
+        @test p2 ∈ C
+        @test div.(p1 + p2, 2) ∈ C
+        @test p1 + 2 * (p2 - p1) ∉ C
+    end
+
+    @testset "triangle" begin
+        rng = MersenneTwister(1)
+        triangle = ConvexHull2D(SVector(Point(1, 1), Point(2, 1), Point(3, 3)))
+        for p in vertices(triangle)
+            @test p ∈ triangle
+        end
+        for i = 1 : 100_000
+            weights = normalize(rand(rng, SVector{3}), 1)
+            p = reduce(+, vertices(triangle) .* weights)
+            @test p ∈ triangle
+        end
+    end
+end
+
+end # module
